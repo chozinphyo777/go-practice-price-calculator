d0702e59ef1222c5b8c88f39fdbc998bec60319f
Cho Zin Phyo <chozinphyo@ucsy.edu.mm>
2026-05-10 12:57:42 +0800

    outsourcing shareable logic into a package

--- prices/prices.go
+++ prices/prices.go
@@ -4,7 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+
+	"gotest.com/price-calculator/conversion"
 )
 
 type TaxIncludedPriceJob struct {
@@ -32,19 +33,15 @@ func (job *TaxIncludedPriceJob) LoadData() {
 		return
 	}
 
-	prices := make([]float64, len(lines))
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-		if err != nil {
-			fmt.Println("Converting price to float failed: ", err)
-			file.Close()
-			return
-		}
-		prices[lineIndex] = floatPrice
-	}
-	file.Close()
-	job.InputPrices = prices
+	prices, err := conversion.StringsToFloats(lines)
 
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+	job.InputPrices = prices
+	file.Close()
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
